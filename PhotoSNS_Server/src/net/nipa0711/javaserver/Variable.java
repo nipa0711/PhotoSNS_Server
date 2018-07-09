796c00d916dc54e0279edaf32b264ba0e5cee087
nipa0711 <nipa0711@gmail.com>
2018-07-09 17:32:45 +0900

    check

---
+++ PhotoSNS_Server/src/net/nipa0711/javaserver/Variable.java
@@ -0,0 +1,8 @@
+package net.nipa0711.javaserver;
+
+public class Variable {
+	final String db_directory = "e:/sqlite/db/";
+	final String db_Name = "photoSNS.db";
+	// HTTP 서버 Port 지정
+	final int port = 1234;
+}
